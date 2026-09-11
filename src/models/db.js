43bf57261b95676813a3400f3c597692ac28112b
precioufabiyi <preciousfabiyi10@gmail.com>
2026-09-12 00:01:17 +0100

    pp

--- src/models/db.js
+++ src/models/db.js
@@ -9,4 +9,16 @@ const pool = new Pool({
     }
 });
 
+const testConnection = async () => {
+    try {
+        const client = await pool.connect();
+        console.log('Database connected successfully.');
+        client.release();
+    } catch (error) {
+        console.error('Database connection failed:', error);
+        throw error;
+    }
+};
+
+export { testConnection };
 export default pool;
